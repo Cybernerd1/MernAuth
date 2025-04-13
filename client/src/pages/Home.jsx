768536f20cdf947d3e848c6df4765c5c12a96143
Ayush Jaiswal <aj85163@gmail.com>
2025-04-13 18:26:39 +0530

    final commit mern Auth Great Stack lecture

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import NavBar from '../components/NavBar';
+import Header from '../components/Header';
+
+const Home = () => {
+    return (
+        <>
+            <div className='flex flex-col items-center justify-center min-h-screen bg-[url("/bg-img.png")] bg-cover bg-center'>
+                <NavBar />
+                <Header />
+            </div>
+
+        </>
+
+    )
+}
+
+export default Home;
